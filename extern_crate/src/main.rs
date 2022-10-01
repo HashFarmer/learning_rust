73082b68781b8c4b99bc8db4bc370645d0fd53bd
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-10-02 01:13:34 +0800

    Update main.rs

--- extern_crate/src/main.rs
+++ extern_crate/src/main.rs
@@ -2,6 +2,8 @@
 //1、在Cargo.toml的dependencies中添加crate名
 //2、use carate_name::
 //不必extern crate crate_name;
+// use是指定某个具体的功能对象，struct 或者 function ？
+// extern crate 引入某一个包，只是一个名空间
 use rand::Rng;
 fn main() {
     let secret_number : u8 = rand::thread_rng().gen_range(1..10);
