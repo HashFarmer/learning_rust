0e02fb89bbd3bd021746b62bf72a6c7135462e82
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-10-30 15:21:34 +0800

    Update ownership.rs

--- ownership.rs
+++ ownership.rs
@@ -14,6 +14,17 @@ when a = b , "copy" or "move"
     let mut _b = b; // copy
     println!("{}", b);
     */
+    
+    // copy within function    
+    /*
+    let a: i32 = 1;
+    fn func(x: i32) -> i32 { x }
+    func(a); // copy
+    println!("{}", a); // ok
+    */
+    
+    
+    
 
     /*
     let mut c = vec![1, 2, 3, 4];
@@ -26,6 +37,17 @@ when a = b , "copy" or "move"
     let mut _d = d;  // move
     //println!("{:?}", d);
     */
+    
+    
+    // move within function
+    /*
+    let s = String::from("hello");
+    fn func(s1: String) -> String {s1};
+    func(s); // move, s被转移给了一个临时变量s1，函数调用完毕后s1销毁了，s也没有了
+    println!("{}", s); // error
+    */
+    
+    
 
 
 2、
